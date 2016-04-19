a9bfa425126c822aced7e0d039ff735aaa339dd9
Patrik Beyls <beylsp@yahoo>
2016-04-19 23:29:05 +0200

    Update api.py

---
+++ appletea/forecastio/__init__.py
@@ -0,0 +1,11 @@
+"""
+The Forecast API allows you to look up the weather anywhere on the globe,
+returning (where available):
+
+- Current conditions
+- Minute-by-minute forecasts out to 1 hour
+- Hour-by-hour forecasts out to 48 hours
+- Day-by-day forecasts out to 7 days
+
+More information: https://developer.forecast.io
+"""
--- appletea/forecastio/api.py
+++ appletea/forecastio/api.py
@@ -1,4 +1,44 @@
-"""
-REST client to forecast.io
+"""Client for forecast.io APIs.
+
+A REST client library for forecast.io APIs.
 """
 import requests
+
+from forecastio.models import Forecast
+
+
+def get_forecast(key, latitude, longitude, **kwargs):
+    """Return weather forecast for a given location.
+
+    Return a weather forecast object for a given location. The key should be
+    your Dark Sky API key, latitude and longitude should be the geographic
+    coordinates of a location in decimal degrees. Additional arguments can be:
+      - units=[setting]: return the API response in units rather than the
+          default Imperial units. Following settings are possible: 'us', 'si',
+          'ca', 'uk2' or 'auto'.
+      - exclude=[blocks]: exclude some number of data blocks from the API
+          response useful for reducing latency and cache space. [blocks]
+          should be a comma-delimited list (without spaces) of any of the
+          following: 'currently', 'minutely', 'hourly', 'daily', 'alerts',
+          'flags'.
+      - extend=hourly: return hourly data for the next seven days, rather then
+          than next two.
+      - lang=[language]: return summary properties in the desired language.
+
+    Args:
+      key: Dark Sky API key.
+      latitude: geographic latitude coordinates in decimal degrees.
+      longitude: geographic longitude coordinated in decimal degrees.
+      kwargs: additional arguments passed to requests.get.
+
+    Returns:
+      A Forecast object with methods for accessing its data.
+    """
+    response = requests.get('https://api.forecast.io/forecast/%s/%s,%s' %
+                            (key, latitude, longitude), params=kwargs)
+    response.raise_for_status()
+
+    json = response.json()
+    headers = response.headers
+
+    return Forecast(json, response, headers)
