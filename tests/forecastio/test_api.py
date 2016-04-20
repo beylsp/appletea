aa67589008d75d1182679ea9122749987c2af96b
beylsp <beylsp@yahoo.com>
2016-04-20 20:00:38 +0000

    Fix typo

--- tests/forecastio/test_api.py
+++ tests/forecastio/test_api.py
@@ -9,7 +9,7 @@ from appletea import forecastio
 @requests_mock.Mocker()
 class TestApi(unittest.TestCase):
 
-    def test_get_forecast_raise_client_error_404(self, mock):
+    def test_get_forecast_raises_client_error_404(self, mock):
         mock.get(requests_mock.ANY,
                  status_code=requests.codes.not_found, json={})
         with self.assertRaises(requests.HTTPError) as e_cm:
@@ -18,7 +18,7 @@ class TestApi(unittest.TestCase):
         self.assertEquals(e_cm.exception.response.status_code, 
                           requests.codes.not_found)
 
-    def test_get_forecast_raise_server_error_503(self, mock):
+    def test_get_forecast_raises_server_error_503(self, mock):
         mock.get(requests_mock.ANY,
                  status_code=requests.codes.service_unavailable, json={})
         with self.assertRaises(requests.HTTPError) as e_cm:
